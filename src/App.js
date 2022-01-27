a5cf33d44afdc39d3ab62ea93f98c3e6b68bfeb6
vagrant <vagrant@vagrant.vm>
2022-01-27 12:39:16 +0000

    menghapus api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Movie from "./Movie.js";
 import React, { useEffect, useState } from "react";
-
+import API_FEATURED from ./Api.js
 const App = () => {
   //disini kita akan melakukan fetch api data dari API TMDB org lalu return sbg json dan di console log
   const [movie, setMovie] = useState([]);
-  const FEATURED_API =
-    "https://api.themoviedb.org/3/discover/movie?api_key=f8a88a605d021d7add571e3ac9dec9aa&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+  
   const IMG_API = null;
   const SEARCH_API = null;
 
