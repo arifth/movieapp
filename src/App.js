e63c2981a136f4566ba0a4d4e8b3c9fe2744f38a
vagrant <vagrant@vagrant.vm>
2022-01-27 12:22:56 +0000

    commit pertama ai

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Movie from './Movie.js';
+import React, {useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => 
+{
+
+
+	//disini kita akan melakukan fetch api data dari API TMDB org lalu return sbg json dan di console log 
+	const [movie,setMovie]= useState([]);
+	const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?api_key=f8a88a605d021d7add571e3ac9dec9aa&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'; 
+	const IMG_API =  null; 
+	const SEARCH_API = null; 
+
+	useEffect(() => {
+		fetch(FEATURED_API)
+			.then(res => res.json())
+			.then(data =>{
+				console.log(data)
+				setMovie(data.results)
+			})
+			
+	},[])
+
+	return (
+		<div>
+			{movie.map((movies) => <Movie key={movies.id} data={movies} />)}
+		</div>	
+	);
 }
+
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,7 @@
+const Movie = () => {
+	return (
+		<p>this is a movie</p>
+	)
+}
+
+export default Movie;
