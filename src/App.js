55bc4a302f01995bcead4b35e8e60cc4f8b3a5ae
vagrant <vagrant@vagrant.vm>
2022-01-27 15:29:46 +0000

    gambar bekerja dengan sempurna

--- src/App.js
+++ src/App.js
@@ -3,27 +3,25 @@ import React, { useEffect, useState } from "react";
 import { api } from "./Api.js";
 const App = () => {
   //disini kita akan melakukan fetch api data dari API TMDB org lalu return sbg json dan di console log
-  const [movie, setMovie] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   const FEATURED_API = api.api;
   //console.log(api.api);
-  const IMG_API = null;
-  const SEARCH_API = null;
 
   useEffect(() => {
     fetch(FEATURED_API)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setMovie(data.results);
-      })``;
+        setMovies(data.results);
+      });
   }, []);
 
   return (
     <div>
-      {movie.map((movies) => (
-        <Movie key={movies.id} data={movies} />
-      ))}
+      {movies.map((movie) => (
+        <Movie key={movie.id} {...movie}/>
+      ))} 
     </div>
   );
 };
--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,14 @@
-const Movie = () => {
-	return (
-		<p>this is a movie</p>
-	)
-}
+import React from "react";
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+console.log(IMG_API);
+const Movie = ({ title, poster_path, overview, vote_average }) => {
+  return (
+    <div className="movie">
+      <img src={IMG_API + poster_path} alt={title} />
+    </div>
+  );
+};
 
 export default Movie;
